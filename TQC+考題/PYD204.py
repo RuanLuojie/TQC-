a392f0c6556e1c4bfb1d5d5336edbf54acd43fb7
RuanLuojie <79133897+RuanLuojie@users.noreply.github.com>
2021-11-10 16:52:54 +0800

    Add files via upload

---
+++ TQC+考題/PYD204.py
@@ -0,0 +1,17 @@
+# TODO
+n1 = eval(input())
+n2 = eval(input())
+op = input()
+
+if op == "+":
+    print(n1 + n2)
+if op == "-":
+    print(n1 - n2)
+if op == "*":
+    print(n1 * n2)
+if op == "/":
+    print(n1 / n2)
+if op == "//":
+    print(n1 // n2)
+if op == "%":
+    print(n1 % n2)
---
+++ TQC+考題/PYD208.py
@@ -0,0 +1,17 @@
+num = eval(input())
+
+# TODO
+if num==10:
+    print('A')
+elif num==11:
+    print('B')
+elif num==12:
+    print('C')
+elif num==13:
+    print('D')
+elif num==14:
+    print('E')
+elif num==15:
+    print('F')
+else:
+    print(num)
